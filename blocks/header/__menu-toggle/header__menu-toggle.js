80625c37ac962968656790e9ccb916a28d96ef86
Aleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-07-26 11:52:02 +0300

    Update header__menu-toggle.js

--- blocks/header/__menu-toggle/header__menu-toggle.js
+++ blocks/header/__menu-toggle/header__menu-toggle.js
@@ -1,9 +1,11 @@
 let menuButton = document.querySelector('.header__menu-toggle');
 
 menuButton.onclick = function(e) {
-    if (menuButton.checked) {
-        menuButton.setAttribute('aria-pressed', true);
-    } else {
-        menuButton.setAttribute('aria-pressed', false);
-    }
+  if (menuButton.checked) {
+      menuButton.setAttribute('aria-pressed', true);
+      menuButton.setAttribute('aria-expanded', true);
+  } else {
+      menuButton.setAttribute('aria-pressed', false);
+      menuButton.setAttribute('aria-expanded', false);
+  }
 }
