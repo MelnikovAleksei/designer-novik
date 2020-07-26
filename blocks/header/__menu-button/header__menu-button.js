c88116de0fe95cf3b84e7b3bd391c08240b9ecbd
Aleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-07-26 12:01:57 +0300

    Update header__menu-button.js

--- blocks/header/__menu-button/header__menu-button.js
+++ blocks/header/__menu-button/header__menu-button.js
@@ -1,7 +1,7 @@
 let menuButton = document.querySelector('.header__menu-button');
 let menuToggle = document.querySelector('.header__menu-toggle');
 menuButton.onclick = function(e) {
-  if (menuToggle.checked) {
+  if (!menuToggle.checked) {
       menuButton.setAttribute('aria-pressed', true);
       menuButton.setAttribute('aria-expanded', true);
   } else {
